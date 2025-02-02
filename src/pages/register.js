ae6259f392c7d5a42d6ecb74ccfd21cdf8171c09
893asphaghona <ricardocandrasulebaganti@gmail.com>
2025-02-03 06:57:47 +0700

    k

--- src/pages/register.js
+++ src/pages/register.js
@@ -12,7 +12,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5001/api/auth/register", {
+      const response = await fetch("https://backend-gerceplaundry.up.railway.app/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password, role }),
