fe9765695e27a4320ace17df9f0ddab59362a808
893asphaghona <ricardocandrasulebaganti@gmail.com>
2025-02-03 05:18:37 +0700

    k

--- src/pages/login.js
+++ src/pages/login.js
@@ -11,7 +11,7 @@ const Login = () => {
     console.log({ email, password });
     e.preventDefault();
     try {
-      const response = await fetch("https://backend-gerceplaundry.up.railway.app/api/auth/login", {
+      const response = await fetch("https://backend-gerceplaundry.up.railway.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
