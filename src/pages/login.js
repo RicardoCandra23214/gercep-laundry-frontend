183d60953c25f04f82ff667b42644fec1b1a39ea
893asphaghona <ricardocandrasulebaganti@gmail.com>
2025-02-03 04:11:03 +0700

    bisa yok

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://backend-gerceplaundry.up.railway.app/api/login", {
+      const response = await fetch("https://backend-gerceplaundry.up.railway.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
